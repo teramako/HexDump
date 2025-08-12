66f5429c909bb471633be98e0f5e89fc3934cc77
teramako <teramako@gmail.com>
2025-08-12 14:40:52 +0000

    :sparkles: [PowerShell] Initial

---
+++ HexDump.psm1
@@ -0,0 +1,29 @@
+using namespace MT.HexDump;
+
+function Write-HexDump
+{
+    [CmdletBinding()]
+    [OutputType([MT.HexDump.CharCollectionRow])]
+    param(
+        [Parameter(ParameterSetName="Data", Mandatory, ValueFromPipeline)]
+        [byte[]] $Data
+        ,
+        [Parameter(ParameterSetName="Stream", Mandatory, ValueFromPipeline)]
+        [System.IO.Stream] $Stream
+        ,
+        [Parameter()]
+        [System.Text.Encoding] $Encoding = [System.Text.Encoding]::UTF8
+    )
+
+    switch ($PSCmdlet.ParameterSetName)
+    {
+        'Data'
+        {
+            Write-Output ([HexDumper]::HexDump($Data, $Encoding))
+        }
+        'Stream'
+        {
+            throw [System.NotSupportedException]::new("Not supported currently");
+        }
+    }
+}
