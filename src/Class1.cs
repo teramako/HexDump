2be1fd13fc505a05e6bcfcd22596b7763f028042
teramako <teramako@gmail.com>
2025-08-15 18:38:48 +0000

    [C#] Debug printを追加

--- src/Class1.cs
+++ src/Class1.cs
@@ -231,10 +231,12 @@ public static class HexDumper
             enc.TryGetChars(bytes, chars, out _);
             if (chars[0] <= 0xFF)
             {
+                DebugPrint($"p={p:X8}: char[0]=0x{(int)chars[0]:X2} [0x00 - 0xFF] bytes=[{string.Join(' ', bytes.Select(static b => $"{b:X2}"))}]", ConsoleColor.Blue);
                 yield return new CharData(bytes[0], offset + p++, (int)chars[0]);
             }
             else if (char.IsSurrogatePair(chars[0], chars[1]))
             {
+                DebugPrint($"p={p:X8}: char=[0x{(int)chars[0]:X2}, 0x{(int)chars[1]:X2}] [SurrogatePair] bytes=[{string.Join(' ', bytes.Select(static b => $"{b:X2}"))}]", ConsoleColor.Blue);
                 yield return new CharData(bytes[0], offset + p++, char.ConvertToUtf32(chars[0], chars[1]));
                 for (var j = 1; j < bytes.Length; j++)
                 {
@@ -244,6 +246,7 @@ public static class HexDumper
             else
             {
                 int byteCount = enc.GetByteCount(chars[..1]);
+                DebugPrint($"p={p:X8}: char[0]=0x{(int)chars[0]:X2} [> 0xFF] bytes=[{string.Join(' ', bytes.Select(static b => $"{b:X2}"))}]", ConsoleColor.Blue);
                 yield return new CharData(bytes[0], offset + p++, (int)chars[0]);
                 for (var j = 1; j < byteCount; j++)
                 {
