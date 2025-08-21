7c77afc4d005c3a83e3760a6425df31b2e096d32
teramako <teramako@gmail.com>
2025-08-21 14:57:45 +0000

    :green_heart: 軽いテスト Latin1, UTF-8, Shift_JIS を追加

--- test/HexDump.Tests.ps1
+++ test/HexDump.Tests.ps1
@@ -9,5 +9,27 @@ Describe 'HexDump' {
 
             $result | Out-String | Write-Host -ForegroundColor DarkGray
         }
+
+        It 'Latin1' {
+            $result = Write-HexDump -Data $script:asciiData -Encoding Latin1
+
+            $result | Out-String | Write-Host -ForegroundColor DarkGray
+        }
+
+        It 'UTF-8' {
+            $data = [System.Text.Encoding]::UTF8.GetBytes('あいうえお')
+
+            $result = Write-HexDump -Data $data -Encoding UTF8
+
+            $result | Out-String | Write-Host -ForegroundColor DarkGray
+        }
+
+        It 'Shift_JIS' {
+            $data = [System.Text.Encoding]::GetEncoding('shift_jis').GetBytes('あいうえお')
+
+            $result = Write-HexDump -Data $data -Encoding Shift_JIS
+
+            $result | Out-String | Write-Host -ForegroundColor DarkGray
+        }
     }
 }
